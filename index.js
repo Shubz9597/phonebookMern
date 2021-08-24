40e628e2294f8e3fae57df43851e521f0603387b
Shubz9597 <shubham.misra95@gmail.com>
2021-08-25 03:13:37 +0530

    Added PUT Route and Updated other routes to support error handling and the backend

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
+const { response } = require('express')
 
 const app = express()
 app.use(express.json())
@@ -33,12 +34,15 @@ app.get('/info', (req, res) => {
     })
 })
 
-app.get('/api/persons/:id' , (req, res) => {
-    Person.findById(req.params.id).then(person => {
-        res.json(person)
-    }).catch(e => {
-        res.status(404).end()
-    })
+app.get('/api/persons/:id' , (req, res, next) => {
+    Person.findById(req.params.id)
+    .then(person => {
+        if(person){
+            res.json(person)
+        } else {
+            res.status(404).end()
+        }
+    }).catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
@@ -55,12 +59,6 @@ app.post('/api/persons', (req, res) => {
         })
     }
 
-    // Person.find({name: body.name}).then(person => {
-    //     return res.status(406).json({
-    //         error: 'Name existing in phonebook, Must be unique'
-    //     })
-    // })
-
     const person = new Person({
         name: body.name,
         number: body.number
@@ -73,12 +71,45 @@ app.post('/api/persons', (req, res) => {
 
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.param.id)
-    persons.filter(p => p.id !== id)
-    
-    res.status(204).end()
+    Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+        res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+        .then(updatedContact => {
+            response.json(updatedContact)
+        })
+        .catch(error => next(error))
+})
+
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({error : 'UnknownEndpoint'})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request , response, next) => {
+    console.log(error)
+
+    if(error.name === 'CastError'){
+        return response.status(400).send({error : 'malformatted id'})
+    }
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
